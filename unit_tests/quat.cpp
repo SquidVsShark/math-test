753a2b75990dd4fc1c41b62275e71e0cbe89fc3c
Phil Cooper-King <phil@cooperking.net>
2016-09-09 13:50:25 +0100

    More mat/quat tests.

--- unit_tests/quat.cpp
+++ unit_tests/quat.cpp
@@ -19,13 +19,54 @@ TEST_CASE("Quat rotations")
     REQUIRE(math::get_y(one_two_three_four) == 0.f);
     REQUIRE(math::get_z(one_two_three_four) == 0.f);
     REQUIRE(math::get_w(one_two_three_four) == 0.f);
-    
-    const math::quat angle_axis = math::quat_init_with_axis_angle(1, 0, 0, math::tau());
-    REQUIRE(math::get_x(angle_axis) == 1.f);
-    REQUIRE(math::get_y(angle_axis) == 0.f);
-    REQUIRE(math::get_z(angle_axis) == 0.f);
-    REQUIRE(math::get_w(angle_axis) == 0.f);
-    
   }
+  
+  SECTION("Angle axis")
+  {
+  }
+  
+  SECTION("Rotation Mat")
+  {
+    // Pitch
+    {
+      const math::quat pitch_rot = math::quat_init_with_axis_angle(1, 0, 0, math::tau());
+      
+      const math::mat3 mat_from_quat = math::quat_get_rotation_matrix(pitch_rot);
+      const math::mat3 mat_data = math::mat3_rotation_pitch_from_euler(math::tau());
+    
+      REQUIRE(math::mat3_equal(mat_from_quat, mat_data));
+    }
+    
+    // Roll
+    {
+      const math::quat roll_rot = math::quat_init_with_axis_angle(0, 0, 1, math::tau());
+      
+      const math::mat3 mat_from_quat = math::quat_get_rotation_matrix(roll_rot);
+      const math::mat3 mat_data = math::mat3_rotation_roll_from_euler(math::tau());
+    
+      REQUIRE(math::mat3_equal(mat_from_quat, mat_data));
+    }
 
+    // Yaw
+    {
+      const math::quat yaw_rot = math::quat_init_with_axis_angle(0, 1, 0, math::tau());
+      
+      const math::mat3 mat_from_quat = math::quat_get_rotation_matrix(yaw_rot);
+      const math::mat3 mat_data = math::mat3_rotation_yaw_from_euler(math::tau());
+    
+      REQUIRE(math::mat3_equal(mat_from_quat, mat_data));
+    }
+  }
+  
+  SECTION("Euler")
+  {
+  }
+  
+  SECTION("Rotation")
+  {
+  }
+  
+  SECTION("Multiplication")
+  {
+  }
 }
